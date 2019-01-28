4183289a16accbe64033036048e49195c7337fa7
Steve Worcester <Worcester.Steve@Gmail.com>
2019-01-28 10:17:33 -0800

    Copying local project to Git

---
+++ PrintEstimator/Calculations.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintEstimator
+{
+    class Calculations
+    {
+        /// <summary>
+        /// Returns the time to accelerate from beginning speed to max speed; can also be used for deceleration
+        /// </summary>
+        /// <param name="acceleration"></param>
+        /// <param name="beginningSpeed"></param>
+        /// <param name="maxSpeed"></param>
+        /// <returns></returns>
+        public double AccelerationTime(double acceleration, double beginningSpeed, double maxSpeed)
+        {
+            var startFromSpeed = maxSpeed - beginningSpeed;
+            var time = startFromSpeed / acceleration;
+            return time;
+        }
+        /// <summary>
+        /// Returns the distance it takes to reach max speed; can also be used for deceleration
+        /// </summary>
+        /// <param name="acceleration"></param>
+        /// <param name="beginningSpeed"></param>
+        /// <param name="time"></param>
+        /// <returns></returns>
+        public double AccelerationDistance(double acceleration, double beginningSpeed, double maxSpeed)
+        {
+            var time = AccelerationTime(acceleration, beginningSpeed, maxSpeed);
+            if (time < 0)
+            {
+                time *= -1;
+            }
+            var startingVelocityTime = beginningSpeed * time;
+            var squareTime = time * time;
+            var accelFromStart = acceleration * .5 * squareTime;
+            var totalDistance = startingVelocityTime + accelFromStart;
+            return totalDistance;
+        }
+        /// <summary>
+        /// Returns the time it takes to travel while going at max speed
+        /// </summary>
+        /// <param name="maxSpeed"></param>
+        /// <param name="distanceInMilimeters"></param>
+        /// <returns></returns>
+        public double MaxSpeedTravelTime(double maxSpeed, double distanceInMilimeters)
+        {
+            return distanceInMilimeters / maxSpeed;
+        }
+    }
+}
---
+++ PrintEstimator/GCodeLogic.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintEstimator
+{
+    enum Movement
+    {
+        G0, // linear move w/ wait
+        G1, // linear move
+        G2, // Clockwise arc move
+        G3, // Counterclockwise arc move
+
+    }
+    class GCodeLogic
+    {
+
+    }
+}
